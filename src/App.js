9316af9ef7a69aa8ed565482f333d1a030582b49
unknown <Nikhil>
2022-12-23 11:09:48 +0530

    hooks folder added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import Resturant from './component/basics/Resturant'
+// import Resturant from './component/basics/Resturant'
+// import UseState from './component/Hooks/UseState'
+import UseEffect from './component/Hooks/UseEffect'
+import UseReducer from './component/Hooks/UseReducer'
 const App = () => {
-  return <Resturant/>
-} 
+  return <UseReducer/>
+}
 export default App
 
